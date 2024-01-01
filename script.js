81e005d6cf08cfcdc501a722b407011d51eb1d65
Meesa000 <asim_99@hotmail.co.uk>
2024-01-01 20:06:49 +0000

    add chop button for tutorial, add logic to cut at 1 second per log

--- script.js
+++ script.js
@@ -1,29 +1,87 @@
 let mainContainer = document.querySelector('.main-container');
 let tutorialLink = document.querySelector('#tutorial-link');
+let tutorialChopWoodBtn = document.querySelector('#tutorial-chop');
+let counter = 0;
+
 
 tutorialLink.addEventListener('click', (event) => {
     event.preventDefault();
     createTutorialContent();
 });
 
+
+
+// function startCounter (func, ms) {
+
+//     setInterval(func, ms);
+//     console.log('Counter: ' + counter);
+//     counter++;
+// }
+
+
 function createTutorialContent (){
 
-    
-
+    // tutorial container
     let tutorialDiv = document.createElement('div');
     tutorialDiv.style.width = '50vw';
-    tutorialDiv.style.height = '15vh';
     tutorialDiv.style.margin = '16px';
     tutorialDiv.style.padding = '16px';
-    tutorialDiv.style.border = '2px solid black'
+    tutorialDiv.style.borderTop = '6px solid black';
     tutorialDiv.style.backgroundColor = '#232a35';
-    tutorialDiv.textContent = 'This is the tutorial content.';
+    tutorialDiv.style.display = 'flex';
+    tutorialDiv.style.flexDirection = 'column';
+    tutorialDiv.style.alignItems = 'center';
+    tutorialDiv.style.gap = '2rem';
+
+
+    // tutorial header
+    let tutorialHeader = document.createElement('div');
+    let text = tutorialHeader.innerHTML = '<h3>Tutorial Island</h3>';
+    text = tutorialHeader.style.color = 'rgb(137, 95, 9)';
+    tutorialHeader.style.whiteSpace = 'pre-line'; // or 'pre-wrap'
+    tutorialHeader.style.textAlign = 'center';
+
+    //tutorial description
+    let tutorialDescription = document.createElement('div');
+    tutorialDescription.innerHTML = 'Welcome to the OSRS idle tutorial island, chop three pieces of wood.'
+
+    // chop button 
+    let tutorialWcContent = document.createElement('div');
+    let tutorialChopWoodBtn = document.createElement('button');
+    tutorialChopWoodBtn.type = 'button';
+    tutorialChopWoodBtn.id = 'tutorial-chop-btn';
+    tutorialChopWoodBtn.innerHTML = 'Chop!';
+    tutorialWcContent.appendChild(tutorialChopWoodBtn);
+    tutorialChopWoodBtn.addEventListener('click', cutWood);
     
+
+    // appends
     mainContainer.appendChild(tutorialDiv);
+    tutorialDiv.appendChild(tutorialHeader);
+    tutorialDiv.appendChild(tutorialDescription);
+    tutorialDiv.appendChild(tutorialWcContent);
+
+    return tutorialChopWoodBtn
+
+
+}
+
+
+function cutWood (){
+    let normalCutRate = 1000; // ms
+    window.setInterval(function(){
+        console.log ('Cut: ' + counter);
+        counter++;
+        
+    },1000);
+    
+
+
 }
 
 
 
-// create tutorial div and append to main containerssss
 
-// create content inside tutorial div, and append it to tutorialdiv 
+
+
+
