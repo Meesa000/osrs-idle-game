a7adda25d5a77c87d2a9ea30dcb4ee278457bb94
Meesa000 <asim_99@hotmail.co.uk>
2024-01-01 21:06:37 +0000

    add logic for tutorial wcing

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
 let mainContainer = document.querySelector('.main-container');
 let tutorialLink = document.querySelector('#tutorial-link');
-let tutorialChopWoodBtn = document.querySelector('#tutorial-chop');
-let counter = 0;
+let logCounter = 0;
 
 
 tutorialLink.addEventListener('click', (event) => {
     event.preventDefault();
-    createTutorialContent();
+    createTutorialDiv();
 });
 
 
-
-// function startCounter (func, ms) {
-
-//     setInterval(func, ms);
-//     console.log('Counter: ' + counter);
-//     counter++;
-// }
-
-
-function createTutorialContent (){
+function createTutorialDiv (){
 
     // tutorial container
     let tutorialDiv = document.createElement('div');
@@ -31,51 +21,74 @@ function createTutorialContent (){
     tutorialDiv.style.display = 'flex';
     tutorialDiv.style.flexDirection = 'column';
     tutorialDiv.style.alignItems = 'center';
-    tutorialDiv.style.gap = '2rem';
-
+    tutorialDiv.style.gap = '1rem';
 
     // tutorial header
     let tutorialHeader = document.createElement('div');
-    let text = tutorialHeader.innerHTML = '<h3>Tutorial Island</h3>';
-    text = tutorialHeader.style.color = 'rgb(137, 95, 9)';
+    tutorialHeader.innerHTML = '<h3>Tutorial Island</h3>';
+    tutorialHeader.style.color = 'rgb(137, 95, 9)';
     tutorialHeader.style.whiteSpace = 'pre-line'; // or 'pre-wrap'
     tutorialHeader.style.textAlign = 'center';
 
-    //tutorial description
+    // tutorial description
     let tutorialDescription = document.createElement('div');
-    tutorialDescription.innerHTML = 'Welcome to the OSRS idle tutorial island, chop three pieces of wood.'
+    tutorialDescription.innerHTML = 'Welcome to the OSRS idle tutorial island, follow the instructions below.'
 
-    // chop button 
+    // tutorial skill header - displays current skill as a header
+    let skillHeader = document.createElement('div');
+    skillHeader.innerHTML = '<h3>Woodcutting</h3>';
+    skillHeader.style.color = 'rgb(137, 95, 9)';
+
+    // tutorial skill description
+    let skillDescription = document.createElement('div');
+    skillDescription.innerHTML = 'First we will start with the woodcutting skill, click the chop button to start chopping wood. Chop 3 logs.';
+
+    // chop button, chop counter
     let tutorialWcContent = document.createElement('div');
     let tutorialChopWoodBtn = document.createElement('button');
+    let tutorialWcCounter = document.createElement('span');
+
+    // container div for chop button and log counter properties
+    tutorialWcContent.style.display = 'flex';
+    tutorialWcContent.style.gap = '1rem';
+
+    // button properties
     tutorialChopWoodBtn.type = 'button';
     tutorialChopWoodBtn.id = 'tutorial-chop-btn';
     tutorialChopWoodBtn.innerHTML = 'Chop!';
-    tutorialWcContent.appendChild(tutorialChopWoodBtn);
-    tutorialChopWoodBtn.addEventListener('click', cutWood);
-    
 
-    // appends
+    // wc counter properties
+    tutorialWcCounter.innerHTML = '<span>Logs chopped: 0</span>';
+    tutorialWcCounter.id = 'tutorial-wc-counter';
+ 
+    // appends for wc div
+    tutorialWcContent.appendChild(tutorialChopWoodBtn);
+    tutorialWcContent.appendChild(tutorialWcCounter);
+    
+    tutorialChopWoodBtn.addEventListener('click', tutorialCutTree);
+    
+    // appends for tutorial div container - contains all tutorial content
     mainContainer.appendChild(tutorialDiv);
     tutorialDiv.appendChild(tutorialHeader);
     tutorialDiv.appendChild(tutorialDescription);
+    tutorialDiv.appendChild(skillHeader);
+    tutorialDiv.appendChild(skillDescription);
     tutorialDiv.appendChild(tutorialWcContent);
-
-    return tutorialChopWoodBtn
-
 
 }
 
-
-function cutWood (){
-    let normalCutRate = 1000; // ms
+// wc rate for cutting tutorial tree
+function tutorialCutTree (){
+    let tutorialWcCounter = document.querySelector('#tutorial-wc-counter');
+    console.log(tutorialWcCounter);
     window.setInterval(function(){
-        console.log ('Cut: ' + counter);
-        counter++;
+        console.log ('Normal logs cut: ' + logCounter);
+        logCounter++;
+        tutorialWcCounter.innerHTML = 'Logs chopped: ' + logCounter;
+
         
     },1000);
     
-
 
 }
 
