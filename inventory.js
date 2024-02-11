de4328049d19591f75d370f4c0d64f6ec995624d
Meesa000 <10476858579+Meesa000@users.noreply.github.com>
2024-02-11 14:23:04 +0000

    fixed formatting of code, bug - invent spaces dont load into dom straight away

--- inventory.js
+++ inventory.js
@@ -5,13 +5,22 @@ const mainHeader = document.querySelector('.header-container-top');
 inventoryLink.addEventListener('click', (event) => {
     event.preventDefault();
     createInventoryGrid();
+    
 });
+
+
+
 
 // Make function that creates the inventory grid
 const inventorySpaces = 28;
 
+
+
 function createInventoryGrid() {
     let inventoryDiv = document.createElement('div');
+    
+
+    
     inventoryDiv.style.width = '50vw';
     inventoryDiv.style.margin = '5px';
     inventoryDiv.style.padding = '16px';
@@ -23,12 +32,7 @@ function createInventoryGrid() {
     inventoryDiv.style.alignItems = 'center';
     inventoryDiv.style.gap = '1rem';
 
-    // Header
-    mainHeader.innerHTML = '<h3>Inventory</h3>';
-   
-     // Append header directly to mainContainer
-
-    // Create inventory spaces
+    const inventoryArray = [];
     for (let i = 0; i < inventorySpaces; i++) {
         let inventorySpace = document.createElement('div');
         inventorySpace.style.width = '60px';
@@ -36,8 +40,42 @@ function createInventoryGrid() {
         inventorySpace.style.display = 'flex';
         inventorySpace.style.flexDirection = 'column';
         inventorySpace.style.border = 'black 2px solid';
+        inventorySpace.className = 'space'+i;
+
         inventoryDiv.appendChild(inventorySpace);
+        inventoryArray.push(inventorySpace);
     }
+    console.log(inventoryArray);
+    
+
+    
+
+    // event listener to add img to bank
+    const space0 = document.querySelector('.space0');
+    if (space0) {
+        space0.addEventListener('click', event => {
+            addBank();
+        });
+    } else {
+        console.error('Element with class .space0 not found in the DOM.');
+    }
+
+
+    // Header
+    mainHeader.innerHTML = '<h3>Inventory</h3>';
+   
+     // Append header directly to mainContainer
+
+    
+    // Create inventory spaces
+    
+    // adds img to bank square
+   function addBank () {
+    let imgElement = document.createElement('img');
+    imgElement.src = 'images/normal-logs.webp';
+    let space0 = document.querySelector('.space0');
+    space0.appendChild(imgElement);
+   }
 
     mainContainer.appendChild(inventoryDiv); // Append inventory grid directly to mainContainer
 }
